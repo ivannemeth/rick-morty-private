19bbe20c2dff4dfef60e3fb9e04f1aff9c459fea
ivannemeth <nemethivn@gmail.com>
2023-11-22 17:28:29 +0100

    cards add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -12,3 +14,34 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+
+    if (response.ok) {
+      // Success (Good Response)
+      const data = await response.json();
+      console.log(data.results);
+      /*const characters = data.result.map((character) => {
+        createCharacterCard(character);
+        cardContainer.innerHTML = "";
+        cardContainer.append(...characters);
+      }); */
+      data.results.forEach((character) => {
+        const characterCard = createCharacterCard(character);
+        // cardContainer.innerHTML = "";
+        cardContainer.append(characterCard);
+        console.log(cardContainer);
+      });
+    } else {
+      // Failure (Bad Response)
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    // Failure (Network error, etc)
+    console.error("An Error occurred");
+  }
+}
+
+fetchCharacters();
